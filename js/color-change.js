2817cd5c5d30673f19dfe7533de136418da8198e
frankabissi <frank@upside.com>
2017-08-17 15:12:03 -0400

    hello

---
+++ js/color-change.js
@@ -0,0 +1,9 @@
+
+    var i=0;
+    function change() {
+      var head=document.getElementById("heado");
+      var color = ["green","blue","red","yellow"];
+      head.style.color=color[i];
+      i=(i+1)%color.length;
+    }
+    setInterval(change,500);
